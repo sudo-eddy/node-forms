014a7b0fc2674a6c083eeedda00bde957a18450b
Eddy Castillo <sudo.coding@protonmail.com>
2019-07-26 06:41:24 -0500

    Add bodyParser extended

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs')
 const middlewares = [
   layout(),
   express.static(path.join(__dirname, 'public')),
-  bodyParser.urlencoded()
+  bodyParser.urlencoded({ extended: true })
 ]
 app.use(middlewares)
 
