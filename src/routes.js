0e373f28dba9fc1c97ca11bcffbdb98683f12c00
Eddy Castillo <sudo.coding@protonmail.com>
2019-07-28 08:02:21 -0500

    Add csrf and helmet

--- src/routes.js
+++ src/routes.js
@@ -13,7 +13,8 @@ router.get('/', (req, res) => {
 router.get('/contact', (req, res) => {
   res.render('contact', {
     data: {},
-    errors: {}
+    errors: {},
+    csrfToken: req.csrfToken()
   })
 })
 
@@ -29,13 +30,15 @@ router.post('/contact', [
     .trim()
     .normalizeEmail()
 ], (req, res) => {
-  const errors = validationResult(req)
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.render('contact', {
       data: req.body,
-      errors: errors.mapped()
+      errors: errors.mapped(),
+      csrfToken: req.csrfToken()
     })
   } 
+  
   const data = matchedData(req)
   console.log('Sanitized:', data)
   //homework: send sanitized data in email or persist in a db
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 
 const path = require('path')
 const express = require('express')
+const helmet = require('helmet')
+const csrf = require('csurf')
 const layout = require('express-layout')
 const bodyParser = require('body-parser')
 const validator = require('express-validator')
@@ -16,6 +18,7 @@ app.set('view engine', 'ejs')
 
 //Set up middlewares
 const middlewares = [
+  helmet(),
   layout(),
   express.static(path.join(__dirname, 'public')),
   bodyParser.urlencoded({ extended: true }),
@@ -28,7 +31,8 @@ const middlewares = [
     saveUninitialized: false,
     cookie: { maxAge: 60000 }
   }),
-  flash()
+  flash(),
+  csrf({ cookie: true })
 ]
 
 app.use(middlewares)
